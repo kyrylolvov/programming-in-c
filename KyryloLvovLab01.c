61da1b84a22ed58f93c4c3453522d093f073c767
Kyrylo Lvov <70720713+kyrylolvov@users.noreply.github.com>
2020-12-06 21:24:57 +0200

    Add files via upload

---
+++ KyryloLvovLab01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int num;
+    printf("please enter a value");
+    scanf("%d", &num);
+
+    if (x = 0)
+        printf("0");
+    else
+        printf("more 0");
+
+        return 0
+}
